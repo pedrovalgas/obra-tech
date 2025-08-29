d4c9c3d25fe8c1f6cf26dec9393187e9ecb3625f
Pedro Lucas <plpvalgas@gmail.com>
2025-08-28 21:58:58 -0300

    Feito as configurações iniciais de dependecias e banco de dados

---
+++ src/main/java/com/pedrolucas/ObraTech/ObraTechApplication.java
@@ -0,0 +1,13 @@
+package com.pedrolucas.ObraTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObraTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObraTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrolucas/ObraTech/ObraTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrolucas.ObraTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObraTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
